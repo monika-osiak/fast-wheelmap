d0fd495ca30a839acef250751dccd5da5760ba87
Monika Osiak <m.osiak46@gmail.com>
2022-05-12 19:04:20 +0200

    Fix heroku db bug

--- api/database.py
+++ api/database.py
@@ -3,14 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import os
+import re
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/fast-wheelmap"
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+uri = os.environ.get('DATABASE_URL')
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    SQLALCHEMY_DATABASE_URL
+    # SQLALCHEMY_DATABASE_URL
+    uri
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
